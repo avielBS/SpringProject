1a6a62dc4293ee1915558c4a425ba9c5dba88d0d
avielBS <aviel2845@gmail.com>
2019-06-07 11:23:49 +0300

    spring application start

--- main/java/springProject/application.java
+++ main/java/springProject/application.java
@@ -1,10 +1,16 @@
 package springProject;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class application {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SpringApplication.run(application.class, args);
+		System.out.println("start");
 	}
 
 }
